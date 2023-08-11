9b6e6801c5503778c356f3fcda741da39a8d4fe2
George Gastaldi <gegastaldi@gmail.com>
2023-08-11 15:15:42 -0300

    Using JDK concurrent class

--- runtime/src/main/java/io/quarkiverse/jca/runtime/QuarkusConnectionManager.java
+++ runtime/src/main/java/io/quarkiverse/jca/runtime/QuarkusConnectionManager.java
@@ -1,7 +1,8 @@
 package io.quarkiverse.jca.runtime;
 
-import io.quarkus.logging.Log;
-import io.vertx.core.impl.ConcurrentHashSet;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+
 import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.resource.ResourceException;
@@ -10,31 +11,35 @@ import jakarta.resource.spi.ConnectionRequestInfo;
 import jakarta.resource.spi.ManagedConnection;
 import jakarta.resource.spi.ManagedConnectionFactory;
 
+import io.quarkus.logging.Log;
+
 /**
  * A {@link ConnectionManager} provides a hook for the resource adapter to pass a connection request to Quarkus
  */
 @ApplicationScoped
 public class QuarkusConnectionManager implements ConnectionManager {
 
-    ConcurrentHashSet<ManagedConnection> connections = new ConcurrentHashSet<>();
+    ConcurrentHashMap<ManagedConnection, Long> connections = new ConcurrentHashMap<>();
 
     @Override
     public Object allocateConnection(ManagedConnectionFactory mcf, ConnectionRequestInfo cxRequestInfo)
             throws ResourceException {
         ManagedConnection mc = mcf.createManagedConnection(null, cxRequestInfo);
-        connections.add(mc);
+        connections.put(mc, System.currentTimeMillis());
         return mc.getConnection(null, cxRequestInfo);
     }
 
-
     @PreDestroy
     public void destroy() {
-        for (ManagedConnection connection : connections) {
+        Iterator<ManagedConnection> iterator = connections.keySet().iterator();
+        while (iterator.hasNext()) {
+            ManagedConnection connection = iterator.next();
             try {
                 connection.destroy();
             } catch (Exception e) {
                 Log.trace("Error while destroying managed connection", e);
             }
+            iterator.remove();
         }
     }
 
