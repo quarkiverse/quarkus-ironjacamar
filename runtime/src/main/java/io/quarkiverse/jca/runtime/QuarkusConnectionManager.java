8e902d56dd941b7f58210bd48625f56872dcbeb1
George Gastaldi <gegastaldi@gmail.com>
2023-08-11 15:11:10 -0300

    Destroy ManagedConnections when shutdown

--- runtime/src/main/java/io/quarkiverse/jca/runtime/QuarkusConnectionManager.java
+++ runtime/src/main/java/io/quarkiverse/jca/runtime/QuarkusConnectionManager.java
@@ -1,5 +1,8 @@
 package io.quarkiverse.jca.runtime;
 
+import io.quarkus.logging.Log;
+import io.vertx.core.impl.ConcurrentHashSet;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.resource.ResourceException;
 import jakarta.resource.spi.ConnectionManager;
@@ -13,10 +16,26 @@ import jakarta.resource.spi.ManagedConnectionFactory;
 @ApplicationScoped
 public class QuarkusConnectionManager implements ConnectionManager {
 
+    ConcurrentHashSet<ManagedConnection> connections = new ConcurrentHashSet<>();
+
     @Override
     public Object allocateConnection(ManagedConnectionFactory mcf, ConnectionRequestInfo cxRequestInfo)
             throws ResourceException {
         ManagedConnection mc = mcf.createManagedConnection(null, cxRequestInfo);
+        connections.add(mc);
         return mc.getConnection(null, cxRequestInfo);
     }
+
+
+    @PreDestroy
+    public void destroy() {
+        for (ManagedConnection connection : connections) {
+            try {
+                connection.destroy();
+            } catch (Exception e) {
+                Log.trace("Error while destroying managed connection", e);
+            }
+        }
+    }
+
 }
