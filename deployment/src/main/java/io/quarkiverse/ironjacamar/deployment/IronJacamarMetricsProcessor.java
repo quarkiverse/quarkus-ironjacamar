6dce6afe7f02dcdd85a12399c303e3337656fd21
George Gastaldi <gegastaldi@gmail.com>
2023-09-11 13:51:25 -0300

    Introduce global metrics flag
    - Fixes #40

--- deployment/src/main/java/io/quarkiverse/ironjacamar/deployment/IronJacamarMetricsProcessor.java
+++ deployment/src/main/java/io/quarkiverse/ironjacamar/deployment/IronJacamarMetricsProcessor.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.ironjacamar.deployment;
 
 import java.util.List;
+import java.util.Optional;
 
 import io.quarkiverse.ironjacamar.runtime.IronJacamarBuildtimeConfig;
 import io.quarkiverse.ironjacamar.runtime.metrics.IronJacamarMetricsRecorder;
@@ -8,6 +9,7 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.metrics.MetricsCapabilityBuildItem;
 import io.quarkus.deployment.metrics.MetricsFactoryConsumerBuildItem;
 
 public class IronJacamarMetricsProcessor {
@@ -17,15 +19,19 @@ public class IronJacamarMetricsProcessor {
     void registerMetrics(IronJacamarMetricsRecorder recorder,
             BuildProducer<MetricsFactoryConsumerBuildItem> metrics,
             List<ContainerCreatedBuildItem> containers,
-            IronJacamarBuildtimeConfig buildtimeConfig) {
-        for (ContainerCreatedBuildItem container : containers) {
-            var config = buildtimeConfig.resourceAdapters().get(container.identifier);
-            if (config == null) {
-                continue;
-            }
-            if (config.ra().cm().pool().metricsEnabled()) {
-                metrics.produce(new MetricsFactoryConsumerBuildItem(
-                        recorder.registerPoolMetrics(container.identifier)));
+            IronJacamarBuildtimeConfig buildtimeConfig,
+            Optional<MetricsCapabilityBuildItem> metricsCapability) {
+        if (buildtimeConfig.metricsEnabled() && metricsCapability.isPresent()) {
+            for (ContainerCreatedBuildItem container : containers) {
+                boolean poolMetricsEnabled = true;
+                var config = buildtimeConfig.resourceAdapters().get(container.identifier);
+                if (config != null) {
+                    poolMetricsEnabled = config.ra().cm().pool().enableMetrics().orElse(true);
+                }
+                if (poolMetricsEnabled) {
+                    metrics.produce(new MetricsFactoryConsumerBuildItem(
+                            recorder.registerPoolMetrics(container.identifier)));
+                }
             }
         }
     }
