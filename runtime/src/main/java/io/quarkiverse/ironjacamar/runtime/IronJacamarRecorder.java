5f50a1c22ef10fb2afda3ddd75a9b393013f7080
George Gastaldi <gegastaldi@gmail.com>
2023-09-11 21:36:31 -0300

    Use IronJacamar's WorkManager and BootstrapContext implementation
    - This gets rid of the original implementation I had for `jakarta.resource.spi.BootstrapContext` and `jakarta.resource.spi.work.WorkManager` and uses the implementations
    provided by IronJacamar
    - Fixes #43
    - Initial work on #42

--- runtime/src/main/java/io/quarkiverse/ironjacamar/runtime/IronJacamarRecorder.java
+++ runtime/src/main/java/io/quarkiverse/ironjacamar/runtime/IronJacamarRecorder.java
@@ -1,18 +1,26 @@
 package io.quarkiverse.ironjacamar.runtime;
 
+import static io.quarkiverse.ironjacamar.Defaults.DEFAULT_BOOTSTRAP_CONTEXT_NAME;
+import static io.quarkiverse.ironjacamar.Defaults.DEFAULT_WORK_MANAGER_NAME;
+
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import jakarta.enterprise.inject.spi.DeploymentException;
 import jakarta.resource.ResourceException;
-import jakarta.resource.spi.XATerminator;
-import jakarta.transaction.TransactionSynchronizationRegistry;
 
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.jboss.jca.core.api.bootstrap.CloneableBootstrapContext;
 import org.jboss.jca.core.api.connectionmanager.ccm.CachedConnectionManager;
+import org.jboss.jca.core.bootstrapcontext.BaseCloneableBootstrapContext;
+import org.jboss.jca.core.bootstrapcontext.BootstrapContextCoordinator;
 import org.jboss.jca.core.connectionmanager.ccm.CachedConnectionManagerImpl;
 import org.jboss.jca.core.spi.transaction.TransactionIntegration;
+import org.jboss.jca.core.workmanager.WorkManagerCoordinator;
+import org.jboss.jca.core.workmanager.WorkManagerImpl;
 
+import io.quarkiverse.ironjacamar.runtime.security.QuarkusSecurityIntegration;
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ArcContainer;
 import io.quarkus.arc.SyntheticCreationalContext;
@@ -65,18 +73,43 @@ public class IronJacamarRecorder {
         };
     }
 
+    public void initDefaultBoostrapContext() {
+        ArcContainer container = Arc.container();
+        TransactionIntegration transactionIntegration = container.select(TransactionIntegration.class).get();
+        BaseCloneableBootstrapContext bootstrapContext = new BaseCloneableBootstrapContext();
+        ManagedExecutor executorService = container.select(ManagedExecutor.class).get();
+
+        // Create WorkManagerImpl
+        WorkManagerImpl workManager = new WorkManagerImpl();
+        workManager.setName(DEFAULT_WORK_MANAGER_NAME);
+        workManager.setSpecCompliant(true);
+        workManager.setSecurityIntegration(new QuarkusSecurityIntegration());
+
+        // TODO: Should we have separate thread pools for short and long running tasks?
+        workManager.setShortRunningThreadPool(executorService);
+        workManager.setLongRunningThreadPool(executorService);
+
+        bootstrapContext.setName(DEFAULT_BOOTSTRAP_CONTEXT_NAME);
+        bootstrapContext.setWorkManager(workManager);
+        bootstrapContext.setWorkManagerName(DEFAULT_WORK_MANAGER_NAME);
+        bootstrapContext.setTransactionSynchronizationRegistry(transactionIntegration.getTransactionSynchronizationRegistry());
+        bootstrapContext.setXATerminator(transactionIntegration.getXATerminator());
+
+        // Register the default in the coordinator
+        WorkManagerCoordinator.getInstance().setDefaultWorkManager(workManager);
+        BootstrapContextCoordinator.getInstance().setDefaultBootstrapContext(bootstrapContext);
+    }
+
     public RuntimeValue<Future<String>> initResourceAdapter(
             String key,
             Supplier<Vertx> vertxSupplier) {
         ArcContainer container = Arc.container();
         Vertx vertx = vertxSupplier.get();
         IronJacamarContainer ijContainer = container.select(IronJacamarContainer.class, Identifier.Literal.of(key)).get();
-        // Lookup JTA beans
-        TransactionSynchronizationRegistry tsr = container.select(TransactionSynchronizationRegistry.class).get();
-        XATerminator xaTerminator = container.select(XATerminator.class).get();
-        IronJacamarVerticle verticle = new IronJacamarVerticle(ijContainer.getResourceAdapter(), tsr, xaTerminator,
-                key,
-                ijContainer.getResourceAdapterFactory().getDescription());
+        CloneableBootstrapContext bootstrapContext = BootstrapContextCoordinator.getInstance().getDefaultBootstrapContext();
+        IronJacamarVerticle verticle = new IronJacamarVerticle(key, ijContainer.getResourceAdapterFactory().getDescription(),
+                ijContainer.getResourceAdapter(),
+                bootstrapContext);
         Future<String> future = vertx.deployVerticle(verticle, new DeploymentOptions()
                 .setWorkerPoolName("jca-worker-pool-" + key)
                 .setWorkerPoolSize(1)
--- runtime/src/main/java/io/quarkiverse/ironjacamar/runtime/IronJacamarVerticle.java
+++ runtime/src/main/java/io/quarkiverse/ironjacamar/runtime/IronJacamarVerticle.java
@@ -2,53 +2,43 @@ package io.quarkiverse.ironjacamar.runtime;
 
 import java.util.Objects;
 
-import jakarta.resource.spi.BootstrapContext;
 import jakarta.resource.spi.ResourceAdapter;
-import jakarta.resource.spi.XATerminator;
-import jakarta.transaction.TransactionSynchronizationRegistry;
+
+import org.jboss.jca.core.api.bootstrap.CloneableBootstrapContext;
+import org.jboss.jca.core.api.workmanager.WorkManager;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
 
 /**
  * A Vert.x {@link io.vertx.core.Verticle} that starts and stops a JCA {@link ResourceAdapter}.
  */
 final class IronJacamarVerticle extends AbstractVerticle {
 
-    private final ResourceAdapter ra;
-    private final TransactionSynchronizationRegistry tsr;
-    private final XATerminator xaTerminator;
     private final String id;
     private final String description;
+    private final ResourceAdapter ra;
+    private final CloneableBootstrapContext bootstrapContext;
 
-    private QuarkusWorkManager workManager;
-
-    public IronJacamarVerticle(ResourceAdapter resourceAdapter, TransactionSynchronizationRegistry tsr,
-            XATerminator xaTerminator, String id, String description) {
-        this.ra = Objects.requireNonNull(resourceAdapter, "resourceAdapter cannot be null");
-        this.tsr = Objects.requireNonNull(tsr, "tsr cannot be null");
-        this.xaTerminator = Objects.requireNonNull(xaTerminator, "xaTerminator cannot be null");
+    public IronJacamarVerticle(String id, String description, ResourceAdapter resourceAdapter,
+            CloneableBootstrapContext bootstrapContext) {
         this.id = Objects.requireNonNull(id, "id cannot be null");
         this.description = Objects.requireNonNull(description, "description cannot be null");
+        this.ra = Objects.requireNonNull(resourceAdapter, "resourceAdapter cannot be null");
+        this.bootstrapContext = Objects.requireNonNull(bootstrapContext, "bootstrapContext cannot be null");
     }
 
     @Override
     public void start() throws Exception {
         QuarkusIronJacamarLogger.log.startingResourceAdapter(id, description);
-        workManager = new QuarkusWorkManager(vertx);
-        // Create BootstrapContext
-        BootstrapContext bootstrapContext = new QuarkusBootstrapContext(workManager, tsr, xaTerminator);
         ra.start(bootstrapContext);
     }
 
     @Override
-    public void stop(Promise<Void> stopPromise) {
+    public void stop() {
         QuarkusIronJacamarLogger.log.stoppingResourceAdapter(id);
-        if (workManager != null) {
-            workManager.close().andThen((v) -> ra.stop()).andThen(stopPromise);
-        } else {
-            ra.stop();
-            stopPromise.complete();
-        }
+        ra.stop();
+        // Shutdown the work manager
+        ((WorkManager) bootstrapContext.getWorkManager()).shutdown();
+        bootstrapContext.shutdown();
     }
 }
