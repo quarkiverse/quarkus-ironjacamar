debc4768de24866e2410cd801654b3fad299d8fc
George Gastaldi <gegastaldi@gmail.com>
2023-11-29 21:04:43 -0300

    Add TransactionRecoveryManager tests

--- integration-tests/artemis-jms/src/test/java/io/quarkiverse/ironjacamar/it/InjectionTest.java
+++ integration-tests/artemis-jms/src/test/java/io/quarkiverse/ironjacamar/it/InjectionTest.java
@@ -1,13 +1,16 @@
 package io.quarkiverse.ironjacamar.it;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.jms.ConnectionFactory;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkiverse.ironjacamar.runtime.IronJacamarContainer;
+import io.quarkiverse.ironjacamar.runtime.TransactionRecoveryManager;
 import io.quarkus.arc.Arc;
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -16,6 +19,9 @@ public class InjectionTest {
 
     @Inject
     ConnectionFactory connectionFactory;
+
+    @Inject
+    Instance<TransactionRecoveryManager> transactionRecoveryManagerInstance;
 
     @Test
     public void testProducer() {
@@ -26,6 +32,17 @@ public class InjectionTest {
     public void shouldInjectConnectionFactory() {
         assertThat(Arc.container().listAll(ConnectionFactory.class)).hasSize(1);
         assertThat(connectionFactory).isNotNull();
+    }
+
+    @Test
+    public void shouldInjectTransactionRecoveryManager() {
+        assertSoftly(softly -> {
+            softly.assertThat(Arc.container().listAll(TransactionRecoveryManager.class)).hasSize(1);
+            softly.assertThat(transactionRecoveryManagerInstance).isNotNull();
+            softly.assertThat(transactionRecoveryManagerInstance.isResolvable()).isTrue();
+            softly.assertThat(transactionRecoveryManagerInstance.get()).isNotNull();
+            softly.assertThat(transactionRecoveryManagerInstance.get().isEnabled()).isTrue();
+        });
     }
 
 }
