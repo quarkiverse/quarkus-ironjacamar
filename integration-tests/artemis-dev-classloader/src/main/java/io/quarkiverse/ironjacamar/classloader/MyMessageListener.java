a577801fe431110e6e811ebb2c2eff5d22066ce1
George Gastaldi <gegastaldi@gmail.com>
2025-02-13 11:07:59 -0300

    Add test for context classloader (#163)

---
+++ integration-tests/artemis-dev-classloader/src/main/java/io/quarkiverse/ironjacamar/classloader/MyMessageListener.java
@@ -0,0 +1,35 @@
+package io.quarkiverse.ironjacamar.classloader;
+
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSProducer;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import jakarta.jms.Queue;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.quarkiverse.ironjacamar.ResourceEndpoint;
+
+@ResourceEndpoint(activationSpecConfigKey = "qa")
+public class MyMessageListener implements MessageListener {
+
+    @Inject
+    @RestClient
+    Instance<MyRestClient> myRestClient;
+
+    @Inject
+    ConnectionFactory connectionFactory;
+
+    @Override
+    public void onMessage(Message message) {
+        try (JMSContext context = connectionFactory.createContext()) {
+            JMSProducer producer = context.createProducer();
+            Queue queue = context.createQueue("reply");
+            // producer.send(queue, myRestClient.get().hello());
+            producer.send(queue, "Hello, World!");
+        }
+    }
+}
